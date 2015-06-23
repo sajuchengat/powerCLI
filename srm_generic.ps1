63ad1972c2db5cb874825f4e4dd635f720b6fbcc
unknown <scnambiar@blr-scnambiar.eng.vmware.com>
2015-06-23 10:56:50 +0530

    first commit

---
+++ srm_generic.ps1
@@ -0,0 +1,46 @@
+# Load the custom functions
+. ./Functions.ps1
+. ./report.ps1
+
+# Connect to protected site VC & SRM
+$creds = Get-Credential
+$vca = Connect-VIServer 10.112.35.255 -Credential $creds
+$srma = Connect-SrmServer -Server $vca -Credential $creds -RemoteCredential $creds
+
+# Output Current SRM Configuration Report
+Get-SrmConfigReport
+
+# get recovery plan
+$rp = Get-RecoveryPlan "rp1"
+
+
+forEach ( $vmlist in Get-ProtectedVM) {
+
+$pvm = $vmlist 
+
+
+# get protected VM
+
+#$pvm =  Get-ProtectedVM | Select -First 1
+
+write-host "Printing pvm $pvm"
+
+# view recovery settings
+$rs = $pvm | Get-RecoverySettings -RecoveryPlan $rp
+
+# update recovery priority
+$rs.RecoveryPriority = "highest"
+$rs.powerOnTimeoutSeconds = "2000"
+
+
+# create new command callout
+$srmCmd = New-SrmCommand -Command 'ping -c 250 localhost' -Description 'Run standard linux failover script' -RunInRecoveredVm
+
+# add command as post recovery command callout
+Add-PostRecoverySrmCommand -RecoverySettings $rs -SrmCommand $srmCmd
+
+# update the recovery settings on the SRM server
+Set-RecoverySettings -ProtectedVm $pvm -RecoveryPlan $rp -RecoverySettings $rs
+
+
+}
